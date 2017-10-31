f11943bd73dbb56a0ccbb3558f8739fccc68dfa6
Mayank Shah <mayank.24shah@gmail.com>
2017-10-31 00:25:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,9 +29,9 @@ function removeElementFromBeginningOfArray(array){
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
-  return array 
+  return array
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0,array.length-1)
-  return array 
+  array.slice(0,array.length-2)
+  return array
 }
