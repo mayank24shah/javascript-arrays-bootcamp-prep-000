37b8121796bba2958e862e56383966d93ba9c12e
Mayank Shah <mayank.24shah@gmail.com>
2017-10-31 00:01:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ function addElementToBeginningOfArray(array, element){
 array.unshift(element)
 return array
 }
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array 
+}
