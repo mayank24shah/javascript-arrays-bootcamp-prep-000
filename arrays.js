4540f095220691a34fdb34ad19f09ab55512f313
Mayank Shah <mayank.24shah@gmail.com>
2017-10-31 00:09:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+function addElementToEndOfArray(array, element){
+  var x = [...array,element]
+  return x 
+}
